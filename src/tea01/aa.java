88ab6f15618c0b436bded9a3fec5e7e8e9044493
zhangshitong <1058609177@qq.com>
2019-05-29 22:19:27 +0800

    update02

--- src/tea01/aa.java
+++ src/tea01/aa.java
@@ -4,5 +4,6 @@ public class aa {
 	public static void main(String[] args) {
 		System.out.println("5216tweyeyiwq163");
 		System.out.println("w83632682");
+		System.out.println("euwey716381");
 	}
 }
